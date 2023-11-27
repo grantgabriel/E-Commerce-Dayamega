9b3667b01c2e938cbab2f95570194a40e93008ba
grantgabriel <grantgabriel30@gmail.com>
2023-11-27 11:05:27 +0700

    Finishes stored function

--- temp/stored-function.sql
+++ temp/stored-function.sql
@@ -1,5 +1,56 @@
-!-- PENTING : Ini Belum ada di Laporan! Stored function untuk mengambil userid dari kurir secara acak.
+-- Menghitung hourly fee kurir -- 2
+DELIMITER &&
+CREATE FUNCTION courierSalary(courier_id CHAR(9)) RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    DECLARE fee DECIMAL(10, 2);
+    SELECT hourly_fee INTO fee FROM couriers WHERE BINARY user_id = BINARY courier_id;
+    RETURN fee * 8; -- Courier work for 8 hours a day!
+END; &&
+DELIMITER ;
 
+-- Menghitung banyak jumlah akun pembeli -- 3
+DELIMITER &&
+CREATE FUNCTION countCustomer() RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE fUser INT;
+    SELECT COUNT(*) INTO fUser FROM users WHERE level_user = 'Users';
+    RETURN fUser;
+END; &&
+DELIMITER ;
+
+-- Menghitung banyak jumlah akun penjual -- 4
+DELIMITER &&
+CREATE FUNCTION countSales() RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE fSales INT;
+    SELECT COUNT(*) INTO fSales FROM users WHERE level_user = 'Sales';
+    RETURN fSales;
+END; &&
+DELIMITER ;
+
+-- Menghitung banyak jumlah akun kurir -- 5
+DELIMITER &&
+CREATE FUNCTION countCouriers() RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE fCourier INT;
+    SELECT COUNT(*) INTO fCourier FROM users WHERE level_user = 'Courier';
+    RETURN fCourier;
+END; &&
+DELIMITER ;
+
+
+
+
+
+!-- PENTING : Ini Belum ada di Laporan! 
+
+-- Stored function untuk menghitung banyak laporan yang belum terselesaikan
+
+-- Stored function untuk mengambil userid dari kurir secara acak.
 DELIMITER &&
 CREATE FUNCTION getRandomCourierUserId()
 RETURNS CHAR(9)
@@ -14,5 +65,22 @@ BEGIN
     LIMIT 1;
 
     RETURN courierId;
+END &&
+DELIMITER ;
+
+-- Stored function untuk menghitung total laba bulan 
+-- JANGAN DISENTUH LAGI KODE INI UDAH BERJALAN YAA ALLAHH
+-- Jangan tanya, biarlah Tuhan dan hanya Tuhan yang paham
+DELIMITER &&
+CREATE FUNCTION monthlyTotal(month_name CHAR(3)) RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    DECLARE profit DECIMAL(10, 2);
+    SET profit = (
+        SELECT SUM(total)
+        FROM orders
+        WHERE DATE_FORMAT(order_date, '%b') = month_name
+    );
+    RETURN COALESCE(profit, 0.00);
 END &&
 DELIMITER ;
